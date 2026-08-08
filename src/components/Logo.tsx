ec1176143e0a7bf94ff5a089bbc899abdbe35592
PF-012 <techfin01.services@gmail.com>
2026-08-08 11:39:56 +0530

    Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ export default function Logo({ className = "w-8 h-8" }: { className?: string }) 
   if (!imgError) {
     return (
       <img 
-        src="/logo.png" 
+        src="/Trans Logo.png" 
         alt="Logo" 
         className={`${className} object-contain`}
         onError={() => setImgError(true)}
