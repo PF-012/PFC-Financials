13bbe828ab025af752d55b64cfe82936d3c20435
PF-012 <techfin01.services@gmail.com>
2026-08-09 12:15:48 +0530

    Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Logo({ className = "w-8 h-8" }: { className?: string }) {
   return (
     <img 
-      src="/Trans Logo.png" 
+      src="/Trans%20Logo.png" 
       alt="Logo" 
       className={`${className} object-contain`}
     />
